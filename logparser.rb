d9fb92c507cb7941adf0f339c54ba2c968906624
Johannes Schriewer <js@planetmutlu.com>
2013-04-19 18:44:06 +0200

    Bugfix: Handle UTF-8 in logfile correctly

--- logparser.rb
+++ logparser.rb
@@ -1,24 +1,30 @@
 #!/usr/bin/env ruby
-puts '<ul>'
+require 'fcntl'
 
 log_garbage = true
-File.readlines('changelog.xml').each { |line|
-	next if line.strip.length == 0
-	if line.match(/^\s+.*$/)
-		line.gsub!('<', '&lt;')
-		line.gsub!('>', '&gt;')
-		if log_garbage
-			puts '<li>' + line
-			log_garbage = false
+fd = STDOUT.fcntl(Fcntl::F_DUPFD)
+out = IO.new(fd, mode: 'w:UTF-8')
+
+out.puts '<ul>'
+File.open('changelog.xml', 'r:UTF-8') do |fp|
+	while line = fp.gets
+		next if line.strip.length == 0
+		if line.match(/^\s+.*$/)
+			line.gsub!('<', '&lt;')
+			line.gsub!('>', '&gt;')
+			if log_garbage
+				out.puts '<li>' + line
+				log_garbage = false
+			else
+				out.puts line
+			end
 		else
-			puts line
-		end
-	else
-		if not log_garbage
-			puts '</li>'
-			log_garbage = true
+			if not log_garbage
+				out.puts '</li>'
+				log_garbage = true
+			end
 		end
 	end
-}
+end
 
-puts '</ul>'
+out.puts '</ul>'
